65fb51107311560767a240af88023acf9c011c38
AndresLince <lynxdarmatron@gmail.com>
2023-07-05 06:22:54 -0500

    Move input page to other file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_calculator/input_page.dart';
 
 void main() {
   runApp(BMICalculator());
@@ -14,25 +15,6 @@ class BMICalculator extends StatelessWidget {
             primary: const Color(0xFF0A0E21),
           )),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      )
     );
   }
 }

e657d0165be33cd6d385e03027a743492f1c5adb
AndresLince <lynxdarmatron@gmail.com>
2023-07-05 05:57:21 -0500

    Add primary and secondary colors

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF0A0E21),
+          secondary: Colors.purple,
+        ),
+      ),
       home: InputPage(),
     );
   }

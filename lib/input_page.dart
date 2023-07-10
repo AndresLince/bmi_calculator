7b45bd4a69d62c277545f1ee5e157415c670b63a
AndresLince <lynxdarmatron@gmail.com>
2023-07-09 21:17:43 -0500

    Move gesturedectore to reusable card class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -32,38 +32,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onTapFunction: () {
                         setState(() {
                           selectedGender = Gender.male;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.male
-                            ? activeCardColor
-                            : inactiveCardColor,
-                        cardChild: GenderWidget(
-                          text: 'MALE',
-                          icon: FontAwesomeIcons.mars,
-                        ),
+                      colour: selectedGender == Gender.male
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: GenderWidget(
+                        text: 'MALE',
+                        icon: FontAwesomeIcons.mars,
                       ),
                     ),
                   ),
                   Expanded(
-                    child: GestureDetector(
-                      onTap: () {
+                    child: ReusableCard(
+                      onTapFunction: () {
                         setState(() {
                           selectedGender = Gender.female;
                         });
                       },
-                      child: ReusableCard(
-                        colour: selectedGender == Gender.female
-                            ? activeCardColor
-                            : inactiveCardColor,
-                        cardChild: GenderWidget(
-                          text: 'FEMALE',
-                          icon: FontAwesomeIcons.venus,
-                        ),
+                      colour: selectedGender == Gender.female
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: GenderWidget(
+                        text: 'FEMALE',
+                        icon: FontAwesomeIcons.venus,
                       ),
                     ),
                   ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final cardChild;
-  ReusableCard({required this.colour, this.cardChild});
-
+  final void Function()? onTapFunction;
+  ReusableCard({required this.colour,this.cardChild, this.onTapFunction});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      decoration:
-      BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
-      child: cardChild,
+    return GestureDetector(
+      onTap: onTapFunction,
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        decoration:
+        BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
+        child: cardChild,
+      ),
     );
   }
 }

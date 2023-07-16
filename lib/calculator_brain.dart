f2b7dee80373633612510f16fd0cac24bba2181b
AndresLince <lynxdarmatron@gmail.com>
2023-07-15 21:53:46 -0500

    Create _bmi private variable

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -3,10 +3,11 @@ import 'dart:math';
 class CalculatorBrain {
   final int height;
   final int weight;
+  double _bmi = 0;
   CalculatorBrain({ required this.height, required this.weight});
 
   String calculateBMI() {
-    double bmi = weight / pow(height / 100, 2);
-    return bmi.toStringAsFixed(1);
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
   }
 }
